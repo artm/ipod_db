793c4396f9804c8bfea67d305a2d752f49426b33
Artem Baguinski <abaguinski@depraktijkindex.nl>
2013-03-03 16:09:14 +0100

    test data (not very good, but it's a start)

---
+++ test_data.rb
@@ -0,0 +1,54 @@
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F00/libgpod191213.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F00/libgpod461353.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F00/libgpod007430.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F00/libgpod773026.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F00/libgpod863057.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F01/libgpod484105.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F01/libgpod805604.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F01/libgpod751619.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F01/libgpod320923.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F01/libgpod442574.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F01/libgpod438233.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F01/libgpod392504.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F01/libgpod496196.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F01/libgpod807107.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F02/sa_p_podcast_130228.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F02/libgpod032874.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F02/libgpod725768.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F02/libgpod176172.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F02/libgpod879833.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F02/libgpod129565.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F02/libgpod588508.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F02/libgpod700862.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F02/libgpod148225.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/F02/libgpod827455.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{file_type: 1, shuffleflag: true, filename: '/iPod_Control/Music/f45/sa_60ssp_podcast_130226.mp3', volume: 100, stoptime: 0, starttime: 0, bookmarkflag: false, skippedcount: 0, playcount: 0, bookmarktime: -1}
+{volume: 20, shuffleflag: false, trackno: 0, shufflepos: 0, trackpos: 15728640}
+{pstate: {volume: 20,shuffleflag: false,trackno: 0,shufflepos: 0,trackpos: 15728640,},tracks: [
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F00/libgpod191213.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F00/libgpod461353.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F00/libgpod007430.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F00/libgpod773026.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F00/libgpod863057.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F01/libgpod484105.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F01/libgpod805604.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F01/libgpod751619.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F01/libgpod320923.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F01/libgpod442574.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F01/libgpod438233.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F01/libgpod392504.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F01/libgpod496196.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F01/libgpod807107.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F02/sa_p_podcast_130228.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F02/libgpod032874.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F02/libgpod725768.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F02/libgpod176172.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F02/libgpod879833.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F02/libgpod129565.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F02/libgpod588508.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F02/libgpod700862.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F02/libgpod148225.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/F02/libgpod827455.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+{file_type: 1,shuffleflag: true,filename: "/iPod_Control/Music/f45/sa_60ssp_podcast_130226.mp3",volume: 100,stoptime: 0,starttime: 0,bookmarkflag: false,skippedcount: 0,playcount: 0,bookmarktime: -1,},
+]
+,}
