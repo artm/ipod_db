4b8e7db5ef4685cdaacad6db0dfd012d119ab199
Artem Baguinski <abaguinski@depraktijkindex.nl>
2013-03-04 07:24:01 +0100

    low level ipod files accessors

--- lib/ipod_db.rb
+++ lib/ipod_db.rb
@@ -1,6 +1,76 @@
+require 'bindata'
+
+class Bool24 < BinData::Primitive
+  uint24le :int
+  def get;   self.int==0 ? false : true ; end
+  def set(v) self.int = v ? 1 : 0 ; end
+end
+
+class Bool8 < BinData::Primitive
+  uint8 :int
+  def get;   self.int==0 ? false : true ; end
+  def set(v) self.int = v ? 1 : 0 ; end
+end
+
+class EncodedString < BinData::Primitive
+  string :str, length: :length
+  def get
+    self.str.force_encoding('UTF-16LE').encode('UTF-8').sub(/\u0000*$/,'')
+  end
+  def set(v)
+    self.str = v.encode('UTF-16LE')
+  end
+end
+
 class IpodDB
   attr_reader :playback_state, :tracks
   def initialize root_dir
     @root_dir
+  end
+
+  class PState < BinData::Record
+    endian :little
+    uint8 :volume, initial_value: 29
+    uint24 :shufflepos
+    uint24 :trackno
+    bool24 :shuffleflag
+    uint24 :trackpos
+    string length: 19
+  end
+
+  class Stats < BinData::Record
+    endian :little
+    uint24 :record_count, value: lambda { records.count }
+    uint24
+    array :records, initial_length: :record_count do
+      uint24 :reclen, value: lambda { num_bytes }
+      int24 :bookmarktime, initial_value: -1
+      string length: 6
+      uint24 :playcount
+      uint24 :skippedcount
+    end
+  end
+
+  class SD < BinData::Record
+    endian :big
+    uint24 :record_count, value: lambda { records.count }
+    uint24 :const, value: 0x10800
+    uint24 :reclen, value: lambda { num_bytes }
+    string length: 9
+    array :records, initial_length: :record_count do
+      uint24 :reclen, value: lambda { num_bytes }
+      string length: 3
+      uint24 :starttime
+      string length: 6
+      uint24 :stoptime
+      string length: 6
+      uint24 :volume, initial_value: 100
+      uint24 :file_type
+      string length: 3
+      encoded_string :filename, length: 522
+      bool8 :shuffleflag
+      bool8 :bookmarkflag
+      string length: 1
+    end
   end
 end
--- spec/ipod_db_spec.rb
+++ spec/ipod_db_spec.rb
@@ -1,11 +1,50 @@
+# encoding: UTF-8
 require 'spec_helper'
 require 'ipod_db'
 
 describe IpodDB do
-  it 'parses database' do
-    expected = eval( File.open( 'test_data.rb' ).read )
-    db = IpodDB.new 'test_data'
-    db.playback_state.must_equal expected[:pstate]
-    db.tracks.must_equal expected[:tracks]
+  before do
+    @expected = eval( File.open( 'test_data.rb' ).read )
   end
+
+  describe IpodDB::PState do
+    it 'parses pstate file' do
+      file = File.open( 'test_data/iPod_Control/iTunes/iTunesPState' )
+      pstate = IpodDB::PState.read(file)
+      @expected[:pstate].each do |field,value|
+        pstate.send(field).value.must_equal value
+      end
+    end
+  end
+
+  describe IpodDB::Stats do
+    it 'parses stats file' do
+      file = File.open( 'test_data/iPod_Control/iTunes/iTunesStats' )
+      stats = IpodDB::Stats.read(file)
+      stats.record_count.must_equal @expected[:tracks].count
+      stats.records.count.must_equal @expected[:tracks].count
+      @expected[:tracks].each_with_index do |track,i|
+        [:bookmarktime, :playcount, :skippedcount].each do |field|
+          stats.records[i].send(field).value.must_equal track[field]
+        end
+      end
+    end
+  end
+
+  describe IpodDB::SD do
+    it 'parses tracks file' do
+      file = File.open( 'test_data/iPod_Control/iTunes/iTunesSD' )
+      tracks = IpodDB::SD.read(file)
+      tracks.record_count.must_equal @expected[:tracks].count
+      tracks.records.count.must_equal @expected[:tracks].count
+      @expected[:tracks].each_with_index do |track,i|
+        [:starttime,:stoptime,:volume,:file_type,:shuffleflag,
+         :bookmarkflag, :filename].each do |field|
+          tracks.records[i].send(field).value.must_equal track[field]
+        end
+      end
+    end
+  end
+
+
 end
