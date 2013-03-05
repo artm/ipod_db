69f8e9e4bc76fceb22b0b434ead40a69c992eb5c
Artem Baguinski <abaguinski@depraktijkindex.nl>
2013-03-05 09:04:09 +0100

    modify $LOAD_PATH in sync-ipod
    1. so sources in lib can require each other more easily
    2. so sync-ipod can be used without bundle exec

--- sync-ipod.rb
+++ sync-ipod.rb
@@ -1,4 +1,10 @@
-require_relative 'lib/ipod_db'
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'bundler/setup'
+
+$LOAD_PATH << File.dirname(__FILE__) + '/lib'
+require 'ipod_db'
 
 root = ARGV[0] || 'test_data'
 db = IpodDB.new root
