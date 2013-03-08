47a23c1281622f2a38864c55a00911d735e3cfa0
Artem Baguinski <abaguinski@depraktijkindex.nl>
2013-03-08 10:29:33 +0100

    includes each/none asserts / expectations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,15 @@
 require 'minitest/autorun'
 require 'purdytest'
 $LOAD_PATH.push "File.dirname(__FILE__)/../lib"
+
+module MiniTest::Assertions
+  def assert_includes_each_of expected, sequence
+    expected.each {|x| assert_includes sequence, x}
+  end
+  def assert_includes_none_of expected, sequence
+    expected.each {|x| refute_includes sequence, x}
+  end
+end
+
+Object.infect_an_assertion :assert_includes_each_of, :must_include_each_of
+Object.infect_an_assertion :assert_includes_none_of, :must_include_none_of
