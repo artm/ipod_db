4a025afde7473f46d68e5e07e98576d4b9b8c015
Artem Baguinski <abaguinski@depraktijkindex.nl>
2013-03-11 21:24:52 +0100

    made sync a separate mode with

--- sync-ipod.rb
+++ sync-ipod.rb
@@ -4,18 +4,8 @@ Main {
   version '0.1.0'
 
   description <<-__
-  Update iPod Shuffle (2nd gen) database. Given directories of bookmarkable
-  and non-bookmarkable media #{program} will find all supported tracks and
-  add them to the iPod database so the device is aware of their existance.
-
-  It is perfectly possible to have other directories full of tracks in device's
-  subconscious - e.g. when time-sharing the device among members of a poor
-  family. Just make sure you update the database using your directories when
-  receiving it from a relation.
-
-  iPod remembers playback position on bookmarkable media and the #{program} goes
-  out of its way to preserve the bookmarks. It also removes bookmarkable files
-  from shuffle list.
+  A couple of tools for working with iPod Shuffle (2nd gen) database from
+  command line
   __
 
   author 'artm <femistofel@gmail.com>'
@@ -25,28 +15,45 @@ Main {
     validate {|path| IpodDB.looks_like_ipod? path}
     description 'path where ipod is mounted'
   }
-  option('books','b') {
-    argument_required
-    default 'books'
-    description 'subdirectory of ipod with bookmarkable media'
-  }
-  option('songs','s') {
-    argument_required
-    default 'songs'
-    description 'subdirectory of ipod with non-bookmarkable media'
-  }
 
-  def run
-    ipod_root = params['ipod_root'].value
-    ipod_db = IpodDB.new ipod_root
-    books_path = File.join ipod_root, params['books'].value
-    songs_path = File.join ipod_root, params['songs'].value
-    books = collect_tracks(books_path, ipod_root)
-    songs = collect_tracks(songs_path, ipod_root)
-    ipod_db.update books: books, songs: songs
-    ipod_db.save
-    exit_success!
-  end
+  mode('sync') {
+    description <<-__
+    Update the iPod database. Given directories of bookmarkable and non-bookmarkable
+    media #{program} will find all supported tracks add them to the iPod database so
+    the device is aware of their existance.
+
+    It is perfectly possible to have other directories full of tracks in device's
+    subconscious - e.g. when time-sharing the device among members of a poor
+    family. Just make sure you update the database using your directories when
+    receiving it from a relation.
+
+    iPod remembers playback position on bookmarkable media and the #{program} goes
+    out of its way to preserve the bookmarks. It also removes bookmarkable files
+    from shuffle list.
+    __
+
+    option('books','b') {
+      argument_required
+      default 'books'
+      description 'subdirectory of ipod with bookmarkable media'
+    }
+    option('songs','s') {
+      argument_required
+      default 'songs'
+      description 'subdirectory of ipod with non-bookmarkable media'
+    }
+    def run
+      ipod_root = params['ipod_root'].value
+      ipod_db = IpodDB.new ipod_root
+      books_path = File.join ipod_root, params['books'].value
+      songs_path = File.join ipod_root, params['songs'].value
+      books = collect_tracks(books_path, ipod_root)
+      songs = collect_tracks(songs_path, ipod_root)
+      ipod_db.update books: books, songs: songs
+      ipod_db.save
+      exit_success!
+    end
+  }
 
   mode('ls') {
     description 'produce a colorful listing of the tracks in the ipod database'
