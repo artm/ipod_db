4d6914efa859295b8038ec99979b8cc0b6be6dc7
Artem Baguinski <abaguinski@depraktijkindex.nl>
2013-04-13 13:58:43 +0200

    refactoring code to smaller simpler functions

--- spec/ipod/track_spec.rb
+++ spec/ipod/track_spec.rb
@@ -2,7 +2,9 @@ require "spec_helper"
 require "ipod/track"
 
 describe Ipod::Track do
+
   describe "#exists?" do
+
     it "knows if file exists" do
       track = Ipod::Track.new filename: "/books/chapter1.mp3", ipod_root: "/mount/ipod"
 
@@ -18,46 +20,56 @@ describe Ipod::Track do
         refute track.exists?
       end
     end
+
   end
 
   describe '#finished?' do
+
     it 'is true if playcount > 0' do
       track = Ipod::Track.new playcount: 1
       assert track.finished?
     end
+
     it 'is true if progress above threshold' do
       track = Ipod::Track.new playcount: 0, bookmarktime: Ipod::Track.sec_to_ticks(99)
       track.stub :length, 100 do
         assert track.finished?
       end
     end
+
     it 'is false if progress below threshold' do
       track = Ipod::Track.new playcount: 0, bookmarktime: Ipod::Track.sec_to_ticks(5)
       track.stub :length, 100 do
         refute track.finished?
       end
     end
+
     it 'is false if progress unknown and playcount is 0' do
       track = Ipod::Track.new playcount: 0
       track.stub :length, 100 do
         refute track.finished?
       end
     end
+
   end
 
   describe '#pos' do
+
     it 'represents bookmarktime in seconds' do
       track = Ipod::Track.new bookmarktime: 1
       assert_in_delta track.pos, Ipod::Track::SecondsPerTick
     end
+
   end
 
   describe '#progress' do
+
     it 'represents progress in track' do
       track = Ipod::Track.new bookmarktime: Ipod::Track.sec_to_ticks(25)
       track.stub :length, 100 do
         assert_in_delta track.progress, 0.25
       end
     end
+
   end
 end
