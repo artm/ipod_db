2783f929f5c2bf34a172b3968f79e83202caa6aa
Artem Baguinski <abaguinski@depraktijkindex.nl>
2013-07-31 08:49:58 +0200

    move the oldest files that fit

--- rsync-podcasts.sh
+++ rsync-podcasts.sh
@@ -1,3 +1,22 @@
-#!/bin/sh
-rsync /home/artm/gPodder/Downloads/ /media/artm/IPOD/books/ -rvu --remove-source-files --include="*.mp3" --include="*/" --exclude="*"
-#find /home/artm/gPodder/Downloads/ -name '*.mp3' -printf '%TY-%Tm-%Td\t%k\t%p\n' | sort
+#!/bin/bash
+
+SRC=/home/artm/gPodder/Downloads
+DEV=/media/artm/IPOD
+DST=$DEV/books
+LIST=./upload-files.txt
+RSYNC_OPTS='-ruvh --remove-source-files'
+
+SPACE=$(df -k $DEV | tail -1 | sed -e 's/ \+/\t/g' | cut -f 4)
+
+# no more than ...
+TOTAL=1000
+find $SRC -name '*.mp3' -printf '%TY-%Tm-%Td\t%k\t%P\n' | sort | cut -f 2,3 | while read SIZE FILE ; do
+  TOTAL=$(($TOTAL+$SIZE))
+  if [ $TOTAL -gt $SPACE ] ; then
+    break
+  fi
+  echo $FILE
+done > $LIST
+
+rsync $RSYNC_OPTS $SRC/ $DST/ --files-from=$LIST
+
